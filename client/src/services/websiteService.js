f8a3e72e02f115797fea31ec089b416658fd5144
kevin <kevinribadiya18@gmail.com>
2026-05-08 18:11:52 +0530

    add some .env and update API urls

--- client/src/services/websiteService.js
+++ client/src/services/websiteService.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+
 export const getUserWebsites = async () => {
   const res = await axios.get(
-    "http://localhost:8080/api/website/user",
+    `${API_URL}/api/website/user`,
     { withCredentials: true }
   );
 
